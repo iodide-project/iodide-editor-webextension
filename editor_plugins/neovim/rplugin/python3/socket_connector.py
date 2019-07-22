b49a67d4f3f53afc7a2406fdb3c2fba613cf0831
Your Name <baylyd@email.arizona.edu>
2019-07-22 15:47:30 -0700

    shifting to buf attach, can't access notification though

--- editor_plugins/neovim/rplugin/python3/socket_connector.py
+++ editor_plugins/neovim/rplugin/python3/socket_connector.py
@@ -62,14 +62,53 @@ class TestPlugin(object):
         ## register the keyboard mapping to send requests to the web browser 
         self.nvim.command("nnoremap <C-e> :EvalRequest<CR>")
 
+        ## register to receive buffer change events
+        ## setup the server
         server = SimpleWebSocketServer('', 9876, SimpleEcho)
         threading.Thread(target=server.serveforever).start()
         ## in background setup timer to send whole buffer to iodide every 5 seconds
         self.server = server
+        ## begin the next message checker
+        ## might have to be in async_call?
+        self.keep_checking = True
+        threading.Thread(target=self.check_messages).start()
+
+    def set_listen(self):
+        self.nvim.request("nvim_buf_attach",1,True,{})
+
+    def next_message(self):
+        response = self.nvim.next_message() 
+        self.nvim.out_write(response +"\n")
+        return response
+
+    def check_messages(self):
+        self.nvim.async_call(self.set_list)
+        while True:
+            ## will block
+            if self.keep_checking:
+                response = self.nvim.async_call(self.next_message)
+                self.nvim.async_call(self.send_whole_document)
+
+            
+
+
+    @pynvim.autocmd('InsertEnter', pattern='*', eval='expand("<afile>")', sync=True)
+    def enter_insert(self,filename):
+        if self.com_clear:
+            self.send_whole_document()
 
     @pynvim.autocmd('InsertLeave', pattern='*', eval='expand("<afile>")', sync=True)
     def exit_insert(self,filename):
-        self.send_whole_document()
+        if self.com_clear:
+            self.send_whole_document()
+
+
+    ## changes that should be like big blocks of lines getting put in and deleted
+    @pynvim.autocmd('TextChanged', pattern='*', eval='expand("<afile>")', sync=True)
+    def on_lines_shift(self,filename):
+        ## if we don't have connection yet
+        if self.com_clear:
+            self.send_whole_document()
 
 
     @pynvim.autocmd('TextChangedI', pattern='*', eval='expand("<afile>")', sync=True)
@@ -85,5 +124,5 @@ class TestPlugin(object):
         character_entered = self.nvim.current.buffer[cursor_position[0]-1][cursor_position[1]-1] ## cursor position's first argument is 1 index based, must go down by one for the active line
         ## the second index is zero based, but the cursor col will have just increased by 1 when the text changes
         ## need to decrease the line cursor position for iodide's translation
-        msg_object = {"pos":[cursor_position[0]-1,cursor_position[1]-1],"type":"INSERT_TEXT","text":character_entered}
+        msg_object = {"pos":[cursor_position[0]-1,cursor_position[1]-1],"type":"INSERT_TEXT","text":line}
         all_connections[-1].sm(json.dumps(msg_object))
